b425f0f62dc578de8371d82e25b561d22bd91c96
Matthew Brecknell <matthew@brecknell.net>
2016-06-15 08:16:58 +1000

    L13: equivalence of classical axioms (solution)

--- submissions/L13-classical-axioms.v
+++ submissions/L13-classical-axioms.v
@@ -6,11 +6,21 @@ Definition imp_disj := forall (P Q: Prop), (P -> Q) -> ~P \/ Q.
 Lemma imp_disj_peirce:
   imp_disj -> peirce.
 Proof.
-
+  unfold peirce, imp_disj.
+  intros H P Q I.
+  destruct (H P P (fun p => p)) as [J|J].
+  apply I; intro K; destruct (J K).
+  apply J.
 Qed.
 
 Lemma peirce_imp_disj:
   peirce -> imp_disj.
 Proof.
-
+  unfold peirce, imp_disj.
+  intros H P Q I.
+  apply (H _ False).
+  intros J.
+  left; intros K.
+  apply J.
+  right; apply I; apply K.
 Qed.
