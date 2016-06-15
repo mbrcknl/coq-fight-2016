4c7d114d16c24e3529d03eff0a0857f1deccf9cb
Matthew Brecknell <matthew@brecknell.net>
2016-06-15 17:11:40 +1000

    L06: difference of squares: remove unnecessary assumptions

--- submissions/L06-difference-of-squares.v
+++ submissions/L06-difference-of-squares.v
@@ -9,17 +9,16 @@ Check mul_sub_distr_l: forall n m p, p * (n - m) = p * n - p * m.
 Check mul_add_distr_r: forall n m p, (n + m) * p = n * p + m * p.
 Check mult_assoc: forall n m p, n * (m * p) = n * m * p.
 Check mult_comm: forall n m, n * m = m * n.
-Check square_le_mono: forall n m, n <= m <-> n * n <= m * m.
 Check mul_1_r: forall n, n * 1 = n.
 
 Lemma difference_of_squares:
-  forall a b, b <= a -> a * a - b * b = (a + b) * (a - b).
+  forall a b, a * a - b * b = (a + b) * (a - b).
 Proof.
 
 Qed.
 
 Lemma difference_of_squares_nested:
-  forall a b, b <= a -> a ^ 4 - b ^ 4 = (a * a + b * b) * (a + b) * (a - b).
+  forall a b, a ^ 4 - b ^ 4 = (a * a + b * b) * (a + b) * (a - b).
 Proof.
 
 Qed.
