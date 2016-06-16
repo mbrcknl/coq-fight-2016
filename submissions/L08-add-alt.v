00f642815ca16b08027705978ef746e5111c590e
Matthew Brecknell <matthew@brecknell.net>
2016-06-16 22:45:00 +1000

    L08: more automation

--- submissions/L08-add-alt.v
+++ submissions/L08-add-alt.v
@@ -9,21 +9,20 @@ Inductive added: nat -> nat -> nat -> Prop :=
 | add_l: forall m n r, added m n r -> added (S m) n (S r)
 | add_r: forall m n r, added m n r -> added m (S n) (S r).
 
+Hint Resolve add_z add_l add_r.
+
 Lemma added_r:
   forall r, added 0 r r.
 Proof.
-  induction r.
-  - apply add_z.
-  - apply add_r; apply IHr.
+  induction r; eauto.
 Qed.
+
+Hint Resolve added_r.
 
 Lemma added_plus:
   forall m n r, added m n r -> m + n = r.
 Proof.
-  induction 1.
-  - reflexivity.
-  - simpl; rewrite IHadded; reflexivity.
-  - rewrite <- plus_n_Sm; rewrite IHadded; reflexivity.
+  induction 1; omega.
 Qed.
 
 Lemma added_plus_iff:
@@ -31,7 +30,5 @@ Lemma added_plus_iff:
 Proof.
   split.
   - apply added_plus.
-  - revert n r; induction m; simpl; intros n r H; subst.
-    + apply added_r.
-    + apply add_l. apply IHm. reflexivity.
+  - revert n r; induction m; simpl; intros n r H; subst; eauto.
 Qed.
