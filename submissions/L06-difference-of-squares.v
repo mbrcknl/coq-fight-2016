f19ab2f2cda773596058b578cad922884edf0784
Matthew Brecknell <matthew@brecknell.net>
2016-06-19 16:45:32 +1000

    L06: split into 3 lemmas, add hints

--- submissions/L06-difference-of-squares.v
+++ submissions/L06-difference-of-squares.v
@@ -2,6 +2,9 @@
 
 Require Import Omega.
 Require Import Arith.
+
+(* If you're still on 8.4 (laggard!), try this instead: *)
+(* Import NPeano.Nat. *)
 Import Nat.
 
 (* Some useful lemmas from the standard library... *)
@@ -11,12 +14,21 @@ Check mult_assoc: forall n m p, n * (m * p) = n * m * p.
 Check mult_comm: forall n m, n * m = m * n.
 Check mul_1_r: forall n, n * 1 = n.
 
+(* None of these lemmas require induction. Just use the library lemmas above. *)
+Lemma squared_squared:
+  forall x, x ^ 4 = (x * x) * (x * x).
+Proof.
+
+Qed.
+
+(* Expand the right hand side first. *)
 Lemma difference_of_squares:
   forall a b, a * a - b * b = (a + b) * (a - b).
 Proof.
 
 Qed.
 
+(* Use both lemmas you just proved. *)
 Lemma difference_of_squares_nested:
   forall a b, a ^ 4 - b ^ 4 = (a * a + b * b) * (a + b) * (a - b).
 Proof.
