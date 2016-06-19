f114284a4c1fde207170c9d68f1ce3f60ffbb977
Matthew Brecknell <matthew@brecknell.net>
2016-06-19 18:37:16 +1000

    L08: simplify

--- submissions/L08-add-alt.v
+++ submissions/L08-add-alt.v
@@ -1,13 +1,13 @@
 (* A strange way to specify addition. *)
 
-Require Import Arith.
-Import Nat.
 Require Import Omega.
 
 Inductive added: nat -> nat -> nat -> Prop :=
 | add_z: added 0 0 0
 | add_l: forall m n r, added m n r -> added (S m) n (S r)
 | add_r: forall m n r, added m n r -> added m (S n) (S r).
+
+Hint Constructors added.
 
 Lemma added_r:
   forall r, added 0 r r.
@@ -21,8 +21,8 @@ Proof.
 
 Qed.
 
-Lemma added_plus_iff:
-  forall m n r, added m n r <-> m + n = r.
+Lemma plus_added:
+  forall m n r, m + n = r -> added m n r.
 Proof.
 
 Qed.
