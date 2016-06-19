ee04ebc95ecd5770255814c8cc4b7ecda03e2f56
Matthew Brecknell <matthew@brecknell.net>
2016-06-19 13:58:13 +1000

    L03: break into 3 lemmas

--- submissions/L03-reverse-abstract.v
+++ submissions/L03-reverse-abstract.v
@@ -3,17 +3,34 @@
 Require Import List.
 Import ListNotations.
 
-Lemma app_tail_nil:
-  forall (T: Type) (xs ys: list T), xs = xs ++ ys -> ys = [].
-Proof.
+Section rev_abstract.
 
-Qed.
+  Variables
+    (T: Type)
+    (f: list T -> list T).
 
-Lemma rev_spec_complete:
-  forall (T: Type) (f: list T -> list T),
-    (forall x, f [x] = [x]) ->
-    (forall xs ys, f (xs ++ ys) = f ys ++ f xs) ->
-    (forall xs, f xs = rev xs).
-Proof.
+  Lemma app_tail_nil:
+    forall (xs ys: list T), xs = xs ++ ys -> ys = [].
+  Proof.
 
-Qed.
+  Qed.
+
+  Hypothesis app_rev: forall xs ys, f (xs ++ ys) = f ys ++ f xs.
+
+  (* Hint: `assert` something which allows you to
+     use a specialisation of app_rev, and then
+     use app_tail_nil. *)
+  Lemma nil_id: f [] = [].
+  Proof.
+
+  Qed.
+
+  Hypothesis single_id: forall x, f [x] = [x].
+
+  Lemma rev_spec_complete:
+    forall xs, f xs = rev xs.
+  Proof.
+
+  Qed.
+
+End rev_abstract.
