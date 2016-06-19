2203bbee45ddb589c9b0ba9a4c7bee86af8c3a91
Matthew Brecknell <matthew@brecknell.net>
2016-06-19 11:26:34 +1000

    L12: split into 3 lemmas

--- submissions/L12-zip-unzip.v
+++ submissions/L12-zip-unzip.v
@@ -3,9 +3,24 @@
 Require Import List.
 Import ListNotations.
 
+Lemma split_length:
+  forall (A B: Type) (ps: list (A * B)) (xs: list A) (ys: list B),
+    split ps = (xs,ys) -> length xs = length ys.
+Proof.
+
+Qed.
+
+(* Try copying the proof from split_length, then modify as needed! *)
 Lemma split_combine:
   forall (A B: Type) (ps: list (A * B)) (xs: list A) (ys: list B),
-    split ps = (xs,ys) <-> length xs = length ys /\ combine xs ys = ps.
+    split ps = (xs,ys) -> combine xs ys = ps.
+Proof.
+
+Qed.
+
+Lemma combine_split:
+  forall (A B: Type) (ps: list (A * B)) (xs: list A) (ys: list B),
+    length xs = length ys -> combine xs ys = ps -> split ps = (xs,ys).
 Proof.
 
 Qed.
