88b331d714f6c07a4a9e621b05785a48790ea50a
Matthew Brecknell <matthew@brecknell.net>
2016-06-19 17:56:26 +1000

    L09: add some hints

--- submissions/L09-le-abstract.v
+++ submissions/L09-le-abstract.v
@@ -15,32 +15,39 @@ Section le_sect.
   (* Additionally impose an ordering between consecutive nats. *)
   Hypothesis le'_S: forall n, le' n (S n).
 
-  (* Show that le' is not so arbitrary after all... *)
+  (* We can show that le' is equivalent to le... *)
+
+  (* Transitivity is probably useful here! *)
   Lemma le'_S_ind:
     forall m n, le' m n -> le' m (S n).
   Proof.
 
   Qed.
 
+  (* Use induction, as well as le'_S_ind. *)
   Lemma le_le':
     forall m n, m <= n -> le' m n.
   Proof.
 
   Qed.
 
+  (* Don't use induction! Use antisymmetry, le_le', and omega. *)
   Lemma le'_0_r:
     forall n, le' n 0 -> n = 0.
   Proof.
 
   Qed.
 
-  (* Hint: start with `destruct (le_ge_dec something something)`. *)
+  (* Tricky, so I've given you a head start. *)
+  (* Don't use induction. Use le_le' and antisymmetry. *)
   Lemma le'_SS:
     forall m n, le' (S m) (S n) -> le' m n.
   Proof.
+    intros m n H; destruct (le_ge_dec (S m) (S n)) as [L|L]; unfold ge in L.
 
   Qed.
 
+  (* Bring it all together. *)
   Lemma le_le'_iff:
     forall m n, m <= n <-> le' m n.
   Proof.
